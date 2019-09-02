b3f1ea10b669d2853283dc0e93fc40b73955e99b
shadepeace <54775670+shadepeace@users.noreply.github.com>
2019-09-02 12:26:48 +0530

    Add files via upload

---
+++ 2.DataTypes.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+
+/********************************************************
+Author      : Wasim Alam (alamwasim225@gmail.com)
+Purpose     : To understant data types
+********************************************************/
+
+void main()
+
+{
+
+    int n1,n2,sum;
+
+
+    printf("\nEnter two numbers : ");
+    scanf("%d %d",&n1,&n2);
+    sum = n1+n2;
+    printf("\nSum of %d and %d is %d",n1,n2,sum);
+
+    getch();
+
+}
